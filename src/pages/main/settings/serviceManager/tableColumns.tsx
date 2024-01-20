c832ce77c02d5ea7ac624e6512dae3f70927cb70
suleyman1406 <dadasovsuleyman126@gmail.com>
2024-01-21 03:40:47 +0400

    ref: service manager name prop update

--- src/pages/main/settings/serviceManager/tableColumns.tsx
+++ src/pages/main/settings/serviceManager/tableColumns.tsx
@@ -1,6 +1,11 @@
-import {GridColDef} from '@mui/x-data-grid';
+import { GridColDef } from "@mui/x-data-grid";
 
 export const columns: GridColDef[] = [
-    { field: 'No', headerName: 'No', flex: 1, headerClassName: 'header-item'},
-    { field: 'type', headerName: 'Xidmət', flex: 1, headerClassName: 'header-item'}
+  { field: "No", headerName: "No", flex: 1, headerClassName: "header-item" },
+  {
+    field: "name",
+    headerName: "Xidmət",
+    flex: 1,
+    headerClassName: "header-item",
+  },
 ];
