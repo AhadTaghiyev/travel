8185e0ed3bc1aea41820c7b8bf41fa68d1eb4544
Suleyman Dadashov <dadasovsuleyman126@gmail.com>
2024-02-13 23:05:37 +0400

    chore: remove unnecessary files

--- src/pages/main/credit/getCredits/schema.tsx
+++
@@ -1,9 +0,0 @@
-import * as Yup from "yup";
-
-export const GetCreditSchema = Yup.object().shape({
-    date: Yup.string().required("Mütləqdir!"),
-    paymentId: Yup.string().required("Mütləqdir!"),
-    source: Yup.string().required("Mütləqdir!"),
-    amount: Yup.number().min(1),
-    note: Yup.string(),
-});
