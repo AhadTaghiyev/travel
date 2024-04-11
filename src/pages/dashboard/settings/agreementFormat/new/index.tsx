397d77b21fa3c3dec7cbfe6d0839e166accb65e5
Suleyman Dadashov <dadasovsuleyman126@gmail.com>
2024-04-12 02:29:16 +0400

    update

--- src/pages/dashboard/settings/agreementFormat/new/index.tsx
+++ src/pages/dashboard/settings/agreementFormat/new/index.tsx
@@ -1,117 +1,132 @@
 // @ts-nocheck
-import { Container } from '@mui/system';
-import Divider from '@mui/material/Divider';
-import {useRef, useState} from 'react';
-import 'draft-js/dist/Draft.css';
-import { Editor } from '@tinymce/tinymce-react';
-import { Button, TextField } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import InputLabel from '@mui/material/InputLabel';
-import { apiService } from '../../../../../server/apiServer';
-import { ToastContainer, toast } from 'react-toastify';
+import { Container } from "@mui/system";
+import Divider from "@mui/material/Divider";
+import { useRef, useState } from "react";
+import "draft-js/dist/Draft.css";
+import { Editor } from "@tinymce/tinymce-react";
+import { Button, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import InputLabel from "@mui/material/InputLabel";
+import { apiService } from "../../../../../server/apiServer";
+import { ToastContainer, toast } from "react-toastify";
 
 const footer = {
-    borderRadius: '2px',
-    background: '#F8F9FB',
-    display: 'flex',
-    justifyContent: 'end',
-    padding: '12px 60px',
-  };
+  borderRadius: "2px",
+  background: "#F8F9FB",
+  display: "flex",
+  justifyContent: "end",
+  padding: "12px 60px",
+};
 
-  const textStyling = {
-    lineHeight: '16px',
-    fontWeight: '400',
-    fontSize: '12px',
-  };
+const textStyling = {
+  lineHeight: "16px",
+  fontWeight: "400",
+  fontSize: "12px",
+};
 
- 
-
-  async function saveData(obj: any){
-    const res = await apiService.post('AgreementFormats/Create', obj)
-    return res;
+async function saveData(obj: any) {
+  const res = await apiService.post("AgreementFormats/Create", obj);
+  return res;
 }
 
 export default function Index() {
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
+  const editorRef = useRef(null);
 
-const editorRef = useRef(null);
-
-const [name, setName] = useState('');
-
+  const [name, setName] = useState("");
 
   const handleSave = async () => {
     if (editorRef.current) {
-        try{
-            const res = await saveData({name: name, text: editorRef.current.getContent()})
-            if(res.status === 200){
-                toast.success('Uğurla yaradıldı!');
-                navigate('/panel/agreementFormats');
-            }else{
-                toast.error('Xəta baş verdi!');
-            }
-        }catch(err){console.error(err)}
-
-        
+      try {
+        const res = await saveData({
+          name: name,
+          text: editorRef.current.getContent(),
+        });
+        if (res.status === 200) {
+          toast.success("Uğurla yaradıldı!");
+          navigate("/panel/agreementFormats");
+        } else {
+          toast.error("Xəta baş verdi!");
+        }
+      } catch (err) {
+        console.error(err);
       }
-  }
+    }
+  };
 
   return (
     <>
-        <Container maxWidth="xl">
-        
-            <InputLabel
-              id="demo-simple-select-label"
-              sx={{ mb: 1 }}
-              style={textStyling}
-            >
-             Müqavilə formatı adı
-            </InputLabel>
-            <TextField
-            required
-              id="outlined-basic"
-              variant="outlined"
-              sx={{ width: '50%', mb: 1 }}
-              value={name}
-              style={textStyling}
-              onChange={(e)=> setName(e.target.value)}
-              size="small"
-            />
-            <Editor
-                apiKey='emglxjwpj34s4n14w7kfzdvj9r5u9wk9ksbgqgn4s8e19wtw'
-                onInit={(evt, editor) => editorRef.current = editor}
-                init={{
-                height: 500,
-                menubar: false,
-                plugins: [
-                    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-                    'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                    'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-                ],
-                toolbar: 'undo redo | blocks | ' +
-                    'bold italic forecolor | alignleft aligncenter ' +
-                    'alignright alignjustify | bullist numlist outdent indent | ' +
-                    'removeformat | help',
-                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-                }}
-            />
-        </Container>
-        <footer style={footer}>
-            <div>
-                <Button
-                variant="contained"
-                color="inherit"
-                sx={{ mr: 2 }}
-                onClick={() => navigate(-1)}
-                >
-                Geri qayıt
-                </Button>
-                <Button variant="contained" onClick={handleSave}>
-                Təsdiqlə
-                </Button>
-            </div>
-        </footer>
-        <ToastContainer position="top-right" autoClose={3000}></ToastContainer>
+      <Container maxWidth="xl">
+        <InputLabel
+          id="demo-simple-select-label"
+          sx={{ mb: 1 }}
+          style={textStyling}
+        >
+          Müqavilə formatı adı
+        </InputLabel>
+        <TextField
+          required
+          id="outlined-basic"
+          variant="outlined"
+          sx={{ width: "50%", mb: 1 }}
+          value={name}
+          style={textStyling}
+          onChange={(e) => setName(e.target.value)}
+          size="small"
+        />
+        <Editor
+          apiKey="ows56ugyfwkmx9qarju0k2ygovl2zyuq5byax7cs5th0cwed"
+          onInit={(evt, editor) => (editorRef.current = editor)}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              "advlist",
+              "autolink",
+              "lists",
+              "link",
+              "image",
+              "charmap",
+              "preview",
+              "anchor",
+              "searchreplace",
+              "visualblocks",
+              "code",
+              "fullscreen",
+              "insertdatetime",
+              "media",
+              "table",
+              "code",
+              "help",
+              "wordcount",
+            ],
+            toolbar:
+              "undo redo | blocks | " +
+              "bold italic forecolor | alignleft aligncenter " +
+              "alignright alignjustify | bullist numlist outdent indent | " +
+              "removeformat | help",
+            content_style:
+              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          }}
+        />
+      </Container>
+      <footer style={footer}>
+        <div>
+          <Button
+            variant="contained"
+            color="inherit"
+            sx={{ mr: 2 }}
+            onClick={() => navigate(-1)}
+          >
+            Geri qayıt
+          </Button>
+          <Button variant="contained" onClick={handleSave}>
+            Təsdiqlə
+          </Button>
+        </div>
+      </footer>
+      <ToastContainer position="top-right" autoClose={3000}></ToastContainer>
     </>
-  )
+  );
 }
--- src/pages/dashboard/settings/agreementFormat/updateAgreement/index.tsx
+++ src/pages/dashboard/settings/agreementFormat/updateAgreement/index.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
-import { useEffect, useState, useRef } from 'react';
-import { Formik, Form } from 'formik';
+import { useEffect, useState, useRef } from "react";
+import { Formik, Form } from "formik";
 import {
   Container,
   Grid,
@@ -8,53 +8,57 @@ import {
   Button,
   TextField,
   FormHelperText,
-} from '@mui/material';
-import { useNavigate,useParams } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import { apiService } from '../../../../../server/apiServer';
-import {IAgreementFormatModel} from '../types';
-import PageTitle from '../../../../../components/pages/pageTitle';
-import { Editor } from '@tinymce/tinymce-react';
-import { agreementBreadCrumb, homeBreadCrumb, updateAgreementBreadCrumb } from '../../../../../routes/breadcrumbs';
+} from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import { apiService } from "../../../../../server/apiServer";
+import { IAgreementFormatModel } from "../types";
+import PageTitle from "../../../../../components/pages/pageTitle";
+import { Editor } from "@tinymce/tinymce-react";
+import {
+  agreementBreadCrumb,
+  homeBreadCrumb,
+  updateAgreementBreadCrumb,
+} from "../../../../../routes/breadcrumbs";
 
 const textStyling = {
-  lineHeight: '16px',
-  fontWeight: '400',
-  fontSize: '12px',
+  lineHeight: "16px",
+  fontWeight: "400",
+  fontSize: "12px",
 };
 
 const footer = {
-  borderRadius: '2px',
-  background: '#F8F9FB',
-  display: 'flex',
-  justifyContent: 'end',
+  borderRadius: "2px",
+  background: "#F8F9FB",
+  display: "flex",
+  justifyContent: "end",
   // width: '100%',
-  padding: '12px 60px',
+  padding: "12px 60px",
 };
 
 const initialValues: IAgreementFormatModel = {
-  name: '',
-  text: '',
+  name: "",
+  text: "",
 };
 
 export default function Index() {
   const navigate = useNavigate();
 
-  const {id} = useParams();
+  const { id } = useParams();
   const editorRef = useRef(null);
 
   const [agreementFormat, setagreementFormat] = useState(initialValues);
 
   useEffect(() => {
     const fetchData = async () => {
-
-      const agreementFormatFromApi = await apiService.get(`AgreementFormats/Get/${id}`);
+      const agreementFormatFromApi = await apiService.get(
+        `AgreementFormats/Get/${id}`
+      );
       setagreementFormat(agreementFormatFromApi.data);
     };
 
     fetchData();
   }, []);
-
 
   return (
     <>
@@ -63,25 +67,24 @@ export default function Index() {
         enableReinitialize={true}
         onSubmit={async (values, { setErrors }) => {
           try {
-            const res = await apiService.put(
-              `/AgreementFormats/Update/${id}`,
-              {...values, text: editorRef.current.getContent()}
-            );
+            const res = await apiService.put(`/AgreementFormats/Update/${id}`, {
+              ...values,
+              text: editorRef.current.getContent(),
+            });
             if (res?.status == 200) {
-              toast.success('Uğurla yaradıldı!');
-              navigate('/panel/agreementformats');
+              toast.success("Uğurla yaradıldı!");
+              navigate("/panel/agreementformats");
             } else {
               setErrors(res.data.errors);
             }
           } catch (err) {
-            toast.error('Xəta baş verdi');
+            toast.error("Xəta baş verdi");
           }
         }}
         render={(props) => (
           <Form>
             <Container maxWidth="xl">
-              <Grid container spacing={4} style={{ marginBottom: '70px' }}>
-          
+              <Grid container spacing={4} style={{ marginBottom: "70px" }}>
                 <Grid item md={3}>
                   <InputLabel
                     id="demo-simple-select-label"
@@ -93,8 +96,8 @@ export default function Index() {
                   <TextField
                     id="outlined-basic"
                     variant="outlined"
-                    sx={{ width: '100%', mb: 1 }}
-                    name={'name'}
+                    sx={{ width: "100%", mb: 1 }}
+                    name={"name"}
                     value={props.values.name}
                     style={textStyling}
                     onChange={props.handleChange}
@@ -102,17 +105,17 @@ export default function Index() {
                   />
                   {props.errors && props.touched.name && (
                     <>
-                      <FormHelperText sx={{ color: 'red' }}>
+                      <FormHelperText sx={{ color: "red" }}>
                         {props.errors.name}
                       </FormHelperText>
-                      <FormHelperText sx={{ color: 'red' }}>
+                      <FormHelperText sx={{ color: "red" }}>
                         {props.errors.name}
                       </FormHelperText>
                     </>
                   )}
                 </Grid>
                 <Grid item md={12}>
-                <InputLabel
+                  <InputLabel
                     id="demo-simple-select-label"
                     sx={{ mb: 1 }}
                     style={textStyling}
@@ -120,32 +123,54 @@ export default function Index() {
                     Müqavilə
                   </InputLabel>
                   <Editor
-                    apiKey='emglxjwpj34s4n14w7kfzdvj9r5u9wk9ksbgqgn4s8e19wtw'
+                    apiKey="ows56ugyfwkmx9qarju0k2ygovl2zyuq5byax7cs5th0cwed"
                     initialValue={props.values.text}
-                    onInit={(evt, editor) => editorRef.current = editor}
-                    onChange={()=> props.handleChange({name: "text", value: editorRef.current.getContent()})}
+                    onInit={(evt, editor) => (editorRef.current = editor)}
+                    onChange={() =>
+                      props.handleChange({
+                        name: "text",
+                        value: editorRef.current.getContent(),
+                      })
+                    }
                     init={{
-                    height: 500,
-                    menubar: false,
-                    plugins: [
-                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-                        'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                        'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-                    ],
-                    toolbar: 'undo redo | blocks | ' +
-                        'bold italic forecolor | alignleft aligncenter ' +
-                        'alignright alignjustify | bullist numlist outdent indent | ' +
-                        'removeformat | help',
-                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                      height: 500,
+                      menubar: false,
+                      plugins: [
+                        "advlist",
+                        "autolink",
+                        "lists",
+                        "link",
+                        "image",
+                        "charmap",
+                        "preview",
+                        "anchor",
+                        "searchreplace",
+                        "visualblocks",
+                        "code",
+                        "fullscreen",
+                        "insertdatetime",
+                        "media",
+                        "table",
+                        "code",
+                        "help",
+                        "wordcount",
+                      ],
+                      toolbar:
+                        "undo redo | blocks | " +
+                        "bold italic forecolor | alignleft aligncenter " +
+                        "alignright alignjustify | bullist numlist outdent indent | " +
+                        "removeformat | help",
+                      content_style:
+                        "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
                     }}
                   />
-    
+
                   {props.errors && props.touched.text && (
                     <>
-                      <FormHelperText sx={{ color: 'red' }}>
+                      <FormHelperText sx={{ color: "red" }}>
                         {props.errors.text}
                       </FormHelperText>
-                      <FormHelperText sx={{ color: 'red' }}>
+                      <FormHelperText sx={{ color: "red" }}>
                         {props.errors.text}
                       </FormHelperText>
                     </>
