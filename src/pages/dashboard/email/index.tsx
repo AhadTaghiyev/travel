749a030613272398be19e2de564c40ac5fec2b09
AhadTaghiyev <taghiyev.ahad@gmail.com>
2024-04-12 00:31:41 +0400

    done

--- src/pages/dashboard/email/index.tsx
+++ src/pages/dashboard/email/index.tsx
@@ -61,8 +61,8 @@ export default function index() {
           touched,
           handleChange,
           handleSubmit,
-          // isSubmitting,
-          // setFieldValue,
+          isSubmitting,
+          setFieldValue,
         }) => (
           <form onSubmit={handleSubmit} className="pt-4">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-4 items-center">
