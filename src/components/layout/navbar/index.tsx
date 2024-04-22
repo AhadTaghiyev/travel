188e1bb624df72971954d8459231d13534e8f9a6
Suleyman Dadashov <dadasovsuleyman126@gmail.com>
2024-04-23 00:45:55 +0400

    navbar responsive design updated

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -2,10 +2,20 @@ import { Link, useNavigate } from "react-router-dom";
 import LogoImg from "@/assets/logo.png";
 import { HashLink } from "react-router-hash-link";
 import { useTranslation } from "react-i18next";
+import { RxHamburgerMenu } from "react-icons/rx";
 import i18n from "i18next";
 import { cn } from "@/lib/utils";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const {
     i18n: { language },
     t,
@@ -25,7 +35,7 @@ const Navbar = () => {
       to: "/#landing-contact-section",
     },
     {
-      label:  t("Blog"),
+      label: t("Blog"),
       to: "#",
     },
     {
@@ -52,7 +62,7 @@ const Navbar = () => {
         <Link to="/" className="text-2xl font-bold">
           <img src={LogoImg} alt="Travacco Logo" />
         </Link>
-        <div className="gap-x-5 md:flex hidden">
+        <div className="gap-x-5 lg:flex hidden">
           {navigations.map((nav, index) => (
             <HashLink
               key={index}
@@ -66,7 +76,7 @@ const Navbar = () => {
         <div className="flex items-center gap-x-3">
           <HashLink
             className={cn(
-              "px-1",
+              "px-1 hidden lg:block",
               language === "en" && "bg-black/30 text-white"
             )}
             onClick={() => {
@@ -78,7 +88,7 @@ const Navbar = () => {
           </HashLink>
           <HashLink
             className={cn(
-              "px-1",
+              "px-1 hidden lg:block",
               language === "az" && "bg-black/30 text-white"
             )}
             onClick={() => {
@@ -90,7 +100,7 @@ const Navbar = () => {
           </HashLink>
           <HashLink
             className={cn(
-              "px-1",
+              "px-1 hidden lg:block",
               language === "ru" && "bg-black/30 text-white"
             )}
             onClick={() => {
@@ -106,6 +116,65 @@ const Navbar = () => {
           >
             {t("Sign in")}
           </Link>
+
+          <Popover open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+            <PopoverTrigger asChild className=" lg:hidden">
+              <Button variant={"outline"}>
+                <RxHamburgerMenu size={28} />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[164px] mr-4 px-2 py-4 lg:hidden">
+              <div className="flex flex-col gap-y-3">
+                {navigations.map((nav, index) => (
+                  <HashLink
+                    key={index}
+                    to={nav.to}
+                    className="text-[#1A202C] font-bold py-1 text-center text-sm hover:text-blue-400 transition"
+                  >
+                    {nav.label}
+                  </HashLink>
+                ))}
+                <div className="flex items-center justify-center gap-x-1 mt-1">
+                  <HashLink
+                    className={cn(
+                      "px-1",
+                      language === "en" && "bg-black/30 text-white"
+                    )}
+                    onClick={() => {
+                      navigate("/en");
+                      changeLanguage("en");
+                    }}
+                  >
+                    EN
+                  </HashLink>
+                  <HashLink
+                    className={cn(
+                      "px-1",
+                      language === "az" && "bg-black/30 text-white"
+                    )}
+                    onClick={() => {
+                      navigate("/");
+                      changeLanguage("az");
+                    }}
+                  >
+                    AZ
+                  </HashLink>
+                  <HashLink
+                    className={cn(
+                      "px-1",
+                      language === "ru" && "bg-black/30 text-white"
+                    )}
+                    onClick={() => {
+                      navigate("/ru");
+                      changeLanguage("ru");
+                    }}
+                  >
+                    RU
+                  </HashLink>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
       </div>
     </div>
